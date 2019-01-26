b7368cc43ec1b21355d1d0ebe2f3cd48b6ba011e
Karlo <Karlo@LAPTOP-27JDMNEL>
2019-01-26 07:04:34 +0100

    Xml citanje gotovo

--- src/pkg4inrow/ReplayRead.java
+++ src/pkg4inrow/ReplayRead.java
@@ -1,8 +1,19 @@
 package pkg4inrow;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -18,28 +29,45 @@ public class ReplayRead implements Runnable{
     
     
     public ReplayRead(List<String> listaStringova, FXMLDocumentController Con){
-        for (int i = 0; i < listaStringova.size(); i++) {
-            String s=listaStringova.get(i);
-            int j=Integer.parseInt(s);
-            lista.add(j);
-        }
         t=new Thread(this);
         t.setDaemon(true);
         con=Con;
         t.start();
-        start();//ucitavanje xml
     }
-
-    ReplayRead() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private void start(){
+    
+    private void readXML() throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder=factory.newDocumentBuilder();
+        Document doc=builder.parse(fileName);
         
+        NodeList potezList=doc.getElementsByTagName("Potez");
+        for (int j = 0; j < potezList.getLength(); j++) {
+            Node p=potezList.item(j);
+            if (p.getNodeType()==Node.ELEMENT_NODE) {
+                Element potez=(Element) p;
+                NodeList kolona=potez.getChildNodes();
+                for (int k = 0; k < kolona.getLength(); k++) {
+                    Node kolonaNode=kolona.item(k);
+                    if (kolonaNode.getNodeType()==Node.ELEMENT_NODE) {
+                        Element kolonaElement=(Element) kolonaNode;
+                        System.out.println(kolonaElement.getTextContent());
+                        lista.add(Integer.parseInt(kolonaElement.getTextContent()));
+                    }
+                }
+            }
+        }
     }
-
     @Override
     public void run() {
+        try {
+            readXML();
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(ReplayRead.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(ReplayRead.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ReplayRead.class.getName()).log(Level.SEVERE, null, ex);
+        }
         i=0;
         try{
             while (i<lista.size()) {
@@ -60,9 +88,4 @@ public class ReplayRead implements Runnable{
             
         }
     }
-    
-    
-
-    
-
 }
